2e3a10595edea3f1ad72cf7837bec05d54f5ef8f
habibaummey78 <habibaummey78@gmail.com>
2023-10-10 22:16:45 -0400

    homework

---
+++ src/gitHub_demo/HelloGitHub.java
@@ -0,0 +1,11 @@
+package gitHub_demo;
+
+public class HelloGitHub {
+	public static void main(String[] args) {
+
+		System.out.println(
+				"I know how to create a separate remote repository in Github and can push \nmy project GithubDemo, without the help of anyone.");
+
+	}
+
+}
